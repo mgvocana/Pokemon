c1ff26788cb7d85a8b4653294b024d73458d1a13
mgvocana <44788832+mgvocana@users.noreply.github.com>
2022-02-17 13:49:55 -0700

    Put in imports

--- src/poke/view/PokePanel.java
+++ src/poke/view/PokePanel.java
@@ -1,5 +1,14 @@
 package poke.view;
 
+import javax.swing.JPanel;
+import javax.swing.JCheckBox;
+import javax.swing.JTextArea;
+import javax.swing.SpringLayout;
+import javax.swing.JComboBox;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
 public class PokePanel
 {
 
