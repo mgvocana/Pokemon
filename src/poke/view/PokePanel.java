9012aae852e08f257de60593c2c390972921b039
mgvocana <44788832+mgvocana@users.noreply.github.com>
2022-02-22 13:26:36 -0700

    Put in better Delcatty and new data members

--- src/poke/view/PokePanel.java
+++ src/poke/view/PokePanel.java
@@ -5,7 +5,9 @@ import javax.swing.JCheckBox;
 import javax.swing.JTextArea;
 import javax.swing.SpringLayout;
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 
@@ -17,6 +19,19 @@ import poke.controller.Controller;
 public class PokePanel extends JPanel
 {
 	private Controller controller;
+	private SpringLayout layout;
+	
+	private JTextField name;
+	private JTextField number;
+	private JTextField health;
+	private JCheckBox canEvolve;
+	private JTextArea typeText;
+	private JScrollPane typePane;
+	private JPanel fieldPanel;
+	
+	private JLabel imageLabel;
+	private ImageIcon pokemonImage;
+	private JComboBox<String> pokedexSelector;
 	
 	public PokePanel(Controller app)
 	{
