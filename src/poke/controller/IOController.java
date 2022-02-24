5f13562ded72b2fc1ca48e7a54890f89eaa451ac
mgvocana <44788832+mgvocana@users.noreply.github.com>
2022-02-24 12:55:51 -0700

    Imports and stubs for IOController

---
+++ src/poke/controller/IOController.java
@@ -0,0 +1,29 @@
+package poke.controller;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+import poke.model.monsters.Pokemon;
+import poke.view.PokeFrame;
+
+public class IOController
+{
+	public static ArrayList<Pokemon> loadData()
+	{
+		ArrayList<Pokemon> pokeList = null;
+		
+		return pokeList;
+	}
+	
+	public static void saveData()
+	{
+		
+	}
+}
