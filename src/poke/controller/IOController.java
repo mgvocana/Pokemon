e65829be5a141cc6348c1c4504d886055370e9f9
mgvocana <44788832+mgvocana@users.noreply.github.com>
2022-02-24 13:05:46 -0700

    Created loadData method

--- src/poke/controller/IOController.java
+++ src/poke/controller/IOController.java
@@ -15,9 +15,25 @@ import poke.view.PokeFrame;
 
 public class IOController
 {
-	public static ArrayList<Pokemon> loadData()
+	public static ArrayList<Pokemon> loadData(String dataFile, PokeFrame frame)
 	{
 		ArrayList<Pokemon> pokeList = null;
+		
+		try (FileInputStream inputStream = new FileInputStream(dataFile); 
+				ObjectInputStream input = new ObjectInputStream(inputStream))
+		{
+			ArrayList <Pokemon> loadedPokemon = new ArrayList<Pokemon>();
+			loadedPokemon = (ArrayList<Pokemon>) input.readObject();
+			pokeList = loadedPokemon;
+		}
+		catch(IOException fileError)
+		{
+			JOptionPane.showMessageDialog(frame, fileError.getMessage(), "Could not read file!", JOptionPane.ERROR_MESSAGE);
+		}
+		catch(ClassNotFoundException classError)
+		{
+			JOptionPane.showMessageDialog(frame, classError.getMessage(), "Incorrect class type!", JOptionPane.ERROR_MESSAGE);
+		}
 		
 		return pokeList;
 	}
